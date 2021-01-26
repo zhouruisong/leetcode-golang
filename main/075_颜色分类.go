c80f8043dce61016f4220790ccf7e9f5db1d373d
Ruisong Zhou <zhouruisong@boe.com.cn>
2021-01-26 09:14:10 +0800

    修改颜色分类通用函数处理

--- main/075_颜色分类.go
+++ main/075_颜色分类.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 /*
 给定一个包含红色、白色和蓝色，一共 n 个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
@@ -25,6 +27,33 @@ import "fmt"
 我们可以考虑对数组进行两次遍历。在第一次遍历中，我们将数组中所有的 00 交换到数组的头部。在第二次遍历中，
 我们将数组中所有的 11 交换到头部的 00 之后。此时，所有的 22 都出现在数组的尾部，这样我们就完成了排序。
 */
+
+/*
+适配各种判断条件
+*/
+//函数指针
+type doFunc func(c, target int) bool
+
+func check(c, target int) bool {
+	return (c % target) == 0
+}
+
+func sortColorsFunc(nums []int) {
+	f := check
+	swapColorsFunc(nums, 2, f)
+	//count0 := swapColorsFunc(nums, 0, f) // 把 0 排到前面
+	//swapColorsFunc(nums[count0:], 1, f)  // nums[:count0] 全部是 0 了，对剩下的 nums[count0:] 把 1 排到前面
+}
+
+func swapColorsFunc(colors []int, target int, f doFunc) (countTarget int) {
+	for i, c := range colors {
+		if f(c, target) {
+			colors[i], colors[countTarget] = colors[countTarget], colors[i]
+			countTarget++
+		}
+	}
+	return
+}
 
 func swapColors(colors []int, target int) (countTarget int) {
 	for i, c := range colors {
@@ -62,6 +91,8 @@ func sortColors2(nums []int) {
 
 func main() {
 	x := []int{2, 0, 2, 1, 1, 0}
-	sortColors(x)
+	//x := []int{2, 3, 2, 1, 1, 4}
+	//sortColors(x)
+	sortColorsFunc(x)
 	fmt.Print(x)
 }
--- main/347_前 K 个高频元素.go
+++ main/347_前 K 个高频元素.go
@@ -1,11 +1,14 @@
 package main
 
-import "container/heap"
+import (
+	"container/heap"
+	"fmt"
+)
 
 /*
 给定一个非空的整数数组，返回其中出现频率前 k 高的元素。
 
- 
+
 
 示例 1:
 
@@ -15,7 +18,7 @@ import "container/heap"
 
 输入: nums = [1], k = 1
 输出: [1]
- 
+
 
 提示：
 
@@ -23,7 +26,7 @@ import "container/heap"
 你的算法的时间复杂度必须优于 O(n log n) , n 是数组的大小。
 题目数据保证答案唯一，换句话说，数组中前 k 个高频元素的集合是唯一的。
 你可以按任意顺序返回答案。
- */
+*/
 
 func topKFrequent(nums []int, k int) []int {
 	/*方法2：使用大根堆, 在map统计完词频后, 放到切片中, 然后用heap.init方法(该方法原理是从底向上建堆,
@@ -84,4 +87,8 @@ func min(a, b int) int {
 		return a
 	}
 	return b
+}
+
+func main() {
+	fmt.Print(topKFrequent([]int{1, 1, 1, 2, 2, 3}, 2))
 }
