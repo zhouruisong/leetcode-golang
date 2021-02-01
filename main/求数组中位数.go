bdc9eac1843e63c9df131b63e2f7497492299bc6
zhouruisong <zhours05@missfresh.cn>
2021-02-01 22:42:11 +0800

    求两个排序数组的中位数

--- main/求数组中位数.go
+++ main/求数组中位数.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	var x []int
@@ -136,6 +139,64 @@ func partion(nums []int, low, high int) int {
 	return i
 }
 
+func findMedianSortedArrays3(nums1 []int, nums2 []int) float64 {
+	//把数组长度小的作为nums1
+	if len(nums1) > len(nums2) {
+		nums1, nums2 = nums2, nums1
+	}
+
+	m := len(nums1)
+	n := len(nums2)
+
+	// 分割线左边的所有元素需要满足的个数 m + (n - m + 1) / 2;
+	totalLeft := (m + n + 1) / 2
+
+	// 在 nums1 的区间 [0, m] 里查找恰当的分割线，
+	// 使得 nums1[i - 1] <= nums2[j] && nums2[j - 1] <= nums1[i]
+	left := 0
+	right := m
+	for left < right {
+		i := left + (right-left+1)/2
+		j := totalLeft - i
+		if nums1[i-1] > nums2[j] {
+			// 下一轮搜索的区间 [left, i - 1]
+			right = i - 1
+		} else {
+			// 下一轮搜索的区间 [i, right]
+			left = i
+		}
+	}
+
+	i := left
+	j := totalLeft - i
+
+	nums1LeftMax := math.MinInt32
+	if i > 0 {
+		nums1LeftMax = nums1[i-1]
+	}
+
+	nums1RightMin := math.MaxInt32
+	if i != m {
+		nums1RightMin = nums1[i]
+	}
+
+	nums2LeftMax := math.MinInt32
+	if j > 0 {
+		nums2LeftMax = nums2[j-1]
+	}
+
+	nums2RightMin := math.MaxInt32
+	if j != n {
+		nums2RightMin = nums2[j]
+	}
+
+	if ((m + n) % 2) == 1 {
+		return math.Max(float64(nums1LeftMax), float64(nums2LeftMax))
+	} else {
+		return (math.Max(float64(nums1LeftMax), float64(nums2LeftMax)) + math.Min(float64(nums1RightMin), float64(nums2RightMin))) / 2
+	}
+}
+
 func main() {
 	//x := []int{1,13,2,55,5,8,7,9}
 	//low := 0
@@ -143,12 +204,12 @@ func main() {
 
 	////快速排序
 	//if low < high {
-		//partion(x, 0, high)
-		//partion(x, 0, mid-1)
-		//partion(x, mid+1, high)
+	//partion(x, 0, high)
+	//partion(x, 0, mid-1)
+	//partion(x, mid+1, high)
 	//}
 
 	//fmt.Println(x)
 	//findMidium(x)
-	findMedianSortedArrays([]int{1, 3}, []int{2})
+	fmt.Println(findMedianSortedArrays3([]int{1, 3}, []int{2}))
 }
