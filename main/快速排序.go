f3008aa1d0dd5ce6d4836dbffd07242ea485540f
zhouruisong <zhours05@missfresh.cn>
2021-02-01 22:10:00 +0800

    快速排序

--- main/快速排序.go
+++ main/快速排序.go
@@ -25,11 +25,39 @@ func QuickSort(arr []int) []int {
 	}
 
 	low, higt = QuickSort(low), QuickSort(higt) //切割递归处理
-	marr := append(append(low, mid...), higt...)
-	return marr
+	res := append(append(low, mid...), higt...)
+	return res
+}
+
+func QuickSort2(arr []int, low, high int) {
+	if low > high {
+		return
+	}
+
+	i := low
+	j := high
+	loc := arr[low]
+
+	for i < j {
+		for i < j && arr[j] >= loc {
+			j--
+		}
+		arr[i], arr[j] = arr[j], arr[i]
+
+		for i < j && arr[i] <= loc {
+			i++
+		}
+		arr[i], arr[j] = arr[j], arr[i]
+	}
+
+	arr[i] = loc
+
+	QuickSort2(arr, 0, i-1)
+	QuickSort2(arr, i+1, high)
 }
 
 func main() {
 	arr := []int{9, 2, 8, 3, 4, 6, 1, 5, 10, 11}
-	fmt.Println(QuickSort(arr))
+	QuickSort2(arr, 0, len(arr)-1)
+	fmt.Println(arr)
 }
