cd839ece6a88a5a59aac81082749411fb3065e4a
Ruisong Zhou <zhouruisong@boe.com.cn>
2021-02-02 10:59:46 +0800

    组合按照框架深度优先遍历

--- main/077_组合.go
+++ main/077_组合.go
@@ -85,6 +85,47 @@ func getCombine2(n int, k int, arr *[]int, res *[][]int, max int) {
 	*arr = res2
 }
 
+func combine3(n int, k int) [][]int {
+	var result [][]int
+	if k == 0 {
+		return result
+	}
+
+	nums := make([]int, n)
+	for i := range nums {
+		nums[i] = i + 1
+	}
+
+	path := []int{}
+	dfs(nums, path, &result, n, k, 0)
+	return result
+}
+
+//N叉树的遍历框架
+func dfs(arr, path []int, res *[][]int, n, k, start int) {
+	//"终止条件" 找到一对组合
+	if k == 0 {
+		//拷贝结果
+		temp := make([]int, len(path))
+		copy(temp, path)
+		*res = append(*res, temp)
+		return
+	}
+
+	//注意这里的i不能从0开始，如果从0开始会出现重复的，比如[1，2]和[2，1]
+	for i := start; i < n-k+1; i++ {
+		//逻辑运算1，（可有可无，视情况而定）
+		//把当前值添加到集合中
+		path = append(path, arr[i])
+		//递归调用，遍历每一个分支
+		dfs(arr, path, res, n, k-1, i+1)
+		//逻辑运算2，（可有可无，视情况而定）
+		//从当前分支跳到下一个分支的时候要把之前添加的值给移除
+		path = path[:len(path)-1]
+	}
+}
+
 func main() {
 	fmt.Println(combine2(4, 2))
+	fmt.Println(combine3(4, 2))
 }
