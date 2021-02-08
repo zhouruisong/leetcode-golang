63367b6d5d56ae7614334b126f302140a6e8a6f3
Ruisong Zhou <zhouruisong@boe.com.cn>
2021-02-08 17:31:31 +0800

    最先公共祖先

--- main/236_二叉树的最近公共祖先.go
+++ main/236_二叉树的最近公共祖先.go
@@ -65,6 +65,23 @@ func lowestCommonAncestor(root, p, q *model.TreeNode) *model.TreeNode {
 	return nil
 }
 
+/*
+    我们想一下，对于根节点 root，p、q 的分布，有两种可能：
+   p、q 分居 root 的左右子树，则 LCA 为 root。
+   p、q 在 root 的同一个子树中，问题转为规模小一点的相同问题。
+   定义递归函数
+   递归函数：返回当前子树中 p 和 q 的 LCA。如果没有 LCA，就返回 null。
+   从根root开始递归搜索，递归搜到各个子树……
+   如果遍历到 p 或 q，比方说 p，则 LCA 要么是当前的 p（q 在 p 的子树中），要么是 p 之上的节点（q 不在 p 的子树中），
+   不可能是 p 之下的节点。不用继续往下遍历，返回当前的 p。
+   当遍历到 null 节点，空树不存在 p 和 q，不存在 LCA，返回 null。
+   当遍历的节点 root 不是 p 或 q 或 null，则递归搜寻 root 的左右子树：
+
+   如果左右子树的递归都有结果，说明 p 和 q 分居 root 的左右子树，返回 root。
+   如果只是一个子树递归调用有结果，说明 p 和 q 都在这个子树，返回该子树递归结果。
+   如果两个子树递归结果都为 null，说明 p 和 q 都不在这俩子树中，返回 null。
+*/
+//递归遍历
 func lowestCommonAncestor2(root, p, q *model.TreeNode) *model.TreeNode {
 	if root == nil || root == p || root == q {
 		return root
