d7a3c8f8140cbdd85613dafa1badcfefcf9dba88
zhouruisong <zhours05@missfresh.cn>
2021-02-19 00:29:32 +0800

    二叉树z字打印

--- main/103_二叉树锯齿形层次遍历.go
+++ main/103_二叉树锯齿形层次遍历.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"container/list"
 	"fmt"
 	"main/model"
 )
@@ -70,10 +71,81 @@ func zigzagLevelOrder(root *model.TreeNode) [][]int {
 	return ret
 }
 
+func zlevelOrder(root *model.TreeNode) [][]int {
+	// write code here
+	//借助队
+	if root == nil {
+		return [][]int{}
+	}
+
+	queueNodes := list.New()
+	queueNodes.PushBack(root)
+	res := [][]int{}
+
+	count := 1
+	for queueNodes.Len() > 0 {
+		n := queueNodes.Len()
+		tmp := []int{}
+		for i := 0; i < n; i++ {
+			node := queueNodes.Remove(queueNodes.Front()).(*model.TreeNode)
+			tmp = append(tmp, node.Val)
+			if node.Left != nil {
+				queueNodes.PushBack(node.Left)
+			}
+			if node.Right != nil {
+				queueNodes.PushBack(node.Right)
+			}
+		}
+		if len(tmp) > 0 {
+			if count%2 == 0 {
+				i := 0
+				j := len(tmp) - 1
+				for i < j {
+					tmp[i], tmp[j] = tmp[j], tmp[i]
+					i++
+					j--
+				}
+			}
+
+			res = append(res, tmp)
+		}
+		count++
+	}
+
+	return res
+}
+
 func main() {
 	root := &model.TreeNode{
 		Val: 5,
 	}
+	/*
+		     5
+		  4     8
+		11   13    4
+	  7	  2      6   1
+
+	*/
 	tree := model.InitBinaryTree(root)
 	fmt.Println(zigzagLevelOrder(tree))
+	fmt.Println(zlevelOrder(gen()))
+}
+
+func gen() *model.TreeNode {
+	root := &model.TreeNode{
+		Val: 5,
+	}
+	root.Left = &model.TreeNode{Val: 4}
+	root.Right = &model.TreeNode{Val: 8}
+
+	root.Left.Left = &model.TreeNode{Val: 11}
+	root.Left.Left.Left = &model.TreeNode{Val: 7}
+	root.Left.Left.Right = &model.TreeNode{Val: 2}
+
+	root.Right.Left = &model.TreeNode{Val: 13}
+	root.Right.Right = &model.TreeNode{Val: 4}
+	root.Right.Right.Left = &model.TreeNode{Val: 6}
+	root.Right.Right.Right = &model.TreeNode{Val: 1}
+
+	return root
 }
