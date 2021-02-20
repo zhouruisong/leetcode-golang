baa53dc7b64703e01cdc4eb79bffa87d9b1b9b16
Ruisong Zhou <zhouruisong@boe.com.cn>
2021-02-20 16:19:29 +0800

    编辑距离

--- main/072_编辑距离.go
+++ main/072_编辑距离.go
@@ -67,10 +67,10 @@ func minDistance(word1 string, word2 string) int {
 	//
 	//D[i][j-1] 为 A 的前 i 个字符和 B 的前 j - 1 个字符编辑距离的子问题。
 	//即对于 B 的第 j 个字符，我们在 A 的末尾添加了一个相同的字符，那么 D[i][j] 最小可以为 D[i][j-1] + 1；
-	//
+
 	//D[i-1][j] 为 A 的前 i - 1 个字符和 B 的前 j 个字符编辑距离的子问题。
 	//即对于 A 的第 i 个字符，我们在 B 的末尾添加了一个相同的字符，那么 D[i][j] 最小可以为 D[i-1][j] + 1；
-	//
+
 	//D[i-1][j-1] 为 A 前 i - 1 个字符和 B 的前 j - 1 个字符编辑距离的子问题。
 	//即对于 B 的第 j 个字符，我们修改 A 的第 i 个字符使它们相同，那么 D[i][j] 最小可以为 D[i-1][j-1] + 1。
 	//特别地，如果 A 的第 i 个字符和 B 的第 j 个字符原本就相同，那么我们实际上不需要进行修改操作。
@@ -90,6 +90,7 @@ func minDistance(word1 string, word2 string) int {
 	*/
 	for i := 1; i <= m; i++ {
 		for j := 1; j <= n; j++ {
+			//最后两个字符相同
 			if word1[i-1] == word2[j-1] {
 				dp[i][j] = dp[i-1][j-1]
 				//fmt.Println("啥都不做", i, j)
